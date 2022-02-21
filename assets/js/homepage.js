e8774ab4df4521c818f48c3b343e8515a7ba9d93
lyndsclev <lyndsclev@gmail.com>
2022-02-20 19:03:26 -0500

    added language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,6 +4,7 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -93,8 +94,33 @@ var displayRepos = function(repos, searchTerm) {
         // append container to the dom 
         repoContainerEl.appendChild(repoEl);
     };
-
-    
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+  };
+
+  var buttonClickHandler = function(event) {
+      var language = event.target.getAttribute("data-language");
+
+      if(language) {
+          getFeaturedRepos(language);
+
+          // clear old content
+          repoContainerEl.textContent = "";
+      }
+  }
+
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
